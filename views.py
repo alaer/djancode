7f92b28c56e43cd3f638bfb2818063c1ac904b20
djancode <re23al@ukr.net>
2010-06-05 12:06:52 +0300

    коментар4

--- views.py
+++ views.py
@@ -8,7 +8,7 @@ import datetime
 
 
 def hello(request):
-    return HttpResponse("qwerty22")
+    return HttpResponse("qwerty123")
 
 def my_homepage_view(request):
     return HttpResponse("My_Home_Page")
